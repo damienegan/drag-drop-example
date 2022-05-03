e9442e2a044489b2da55f78e7e2c348d962f2c2a
Damien Egan <damien@pashn.no>
2022-05-03 16:08:23 +0200

    multiple updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,133 +16,139 @@ export class AppComponent {
   totalPointsSemester5 = 0;
   totalPointsSemester6 = 0;
 
+
+  availableCourses = [
+    {
+      code: 'Int',
+      title: 'Internship',
+      points: 15
+    },
+    {
+      code: 'EDI XX06',
+      title: 'Business and Systems Architecture',
+      points: 7.5
+    },
+    {
+      code: 'EDI XX07',
+      title: 'Digital Business Analysis ',
+      points: 7.5
+    },
+    {
+      code: 'EDI XXXX',
+      title: 'Digital Innovation',
+      points: 7.5
+    }
+  ];
+
+
   semester1 = [
     {
-      name: 'EXC 3410',
-      category: 'The Firm',
+      code: 'EXC 3410',
+      title: 'The Firm',
       points: 7.5
     },
     {
-      name: 'EXC 3452',
-      category: 'Financial Reporting and Analysis',
+      code: 'EXC 3452',
+      title: 'Financial Reporting and Analysis',
       points: 7.5
     },
     {
-      name: 'EXC 3415',
-      category: 'Marketing Management',
+      code: 'EXC 3415',
+      title: 'Marketing Management',
       points: 7.5
     },
     {
-      name: 'EDI XX01',
-      category: 'Programming and Data Management',
+      code: 'EDI XX01',
+      title: 'Programming and Data Management',
       points: 7.5
     }
   ];
   semester2 = [
     {
-      name: 'EXC 3442',
-      category: 'Managerial Accounting',
+      code: 'EXC 3442',
+      title: 'Managerial Accounting',
       points: 7.5
     },
     {
-      name: 'EXC 2910',
-      category: 'Mathematics',
+      code: 'EXC 2910',
+      title: 'Mathematics',
       points: 7.5
     },
     {
-      name: 'EDI XX02',
-      category: 'Digital Value Creation',
+      code: 'EDI XX02',
+      title: 'Digital Value Creation',
       points: 7.5
     },
     {
-      name: 'EXC 2904',
-      category: 'Statistics',
+      code: 'EXC 2904',
+      title: 'Statistics',
       points: 7.5
     }
   ];
   semester3 = [
     {
-      name: 'EXC 3520',
-      category: 'Microeconomics',
+      code: 'EXC 3520',
+      title: 'Microeconomics',
       points: 7.5
     },
     {
-      name: 'EDI XX03',
-      category: 'Digital Problem Solving and Task Automation',
+      code: 'EDI XX03',
+      title: 'Digital Problem Solving and Task Automation',
       points: 7.5
     },
     {
-      name: 'EDI XX04',
-      category: 'Influential Communication for Digital Business',
+      code: 'EDI XX04',
+      title: 'Influential Communication for Digital Business',
       points: 7.5
     },
     {
-      name: 'EDI XX05',
-      category: 'Organizing Digitally',
+      code: 'EDI XX05',
+      title: 'Organizing Digitally',
       points: 7.5
     }
   ];
 
   semester4 = [
     {
-      name: 'EXC 3525',
-      category: 'Macroeconomics',
-      points: 15
-    },
-    {
-      name: 'EXC 3505',
-      category: 'Strategy',
+      code: 'EXC 3525',
+      title: 'Macroeconomics',
       points: 7.5
     },
     {
-      name: 'EXC 2953',
-      category: 'Organizational Behaviour and Management',
+      code: 'EXC 3505',
+      title: 'Strategy',
       points: 7.5
     },
     {
-      name: 'EDI XX06',
-      category: 'Business and Systems Architecture',
+      code: 'EXC 2953',
+      title: 'Organizational Behaviour and Management',
       points: 7.5
     }
+
   ];
 
   semester5 = [
     {
-      name: 'EDI XX07',
-      category: 'Digital Business Analysis ',
-      points: 7.5
-    },
-    {
-      name: 'EDI XXXX',
-      category: 'Digital Innovation',
-      points: 7.5
-    },
-    {
-      name: 'Int 1234',
-      category: 'Internship',
-      points: 15
-    },
-    {
-      name: 'EXC',
-      category: 'Exchange',
+      code: 'EXC',
+      title: 'Exchange',
       points: 15
     }
   ];
 
   semester6 = [
     {
-      name: 'EDI XX09',
-      category: 'Digital Business Law',
+      code: 'EDI XX09',
+      title: 'Digital Business Law',
       points: 7.5
     },
     {
-      name: 'EDI XX10',
-      category: 'Digital Business Project',
+      code: 'EDI XX10',
+      title: 'Digital Business Project',
       points: 15
     },
     {
-      name: 'ELE 1234',
-      category: 'Elective',
+      code: 'ELE 1234',
+      title: 'Elective',
       points: 7.5
     },
 
@@ -153,6 +159,7 @@ export class AppComponent {
       moveItemInArray(event.container.data,
         event.previousIndex,
         event.currentIndex);
+
     } else {
       transferArrayItem(event.previousContainer.data,
         event.container.data,
